5bb4ce7eb5f7d2d1555c5426d8a70d112de5f026
sharon17kevin <sharon17kevin@gmail.com>
2025-08-06 17:35:14 +0100

    Refactor the Navbar code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,14 +32,14 @@ const Navbar = () => {
       }}
       animate={hidden ? "hidden" : "visible"}
       transition={{ duration: 0.35, ease: "easeInOut" }}
-      className="w-full z-50 bg-primary text-white sticky top-0 flex"
+      className="w-full z-50 bg-white border-b border-gray-200  sticky top-0 flex"
     >
       <div className=" xl:max-w-[90vw] w-full mx-auto px-4 sm:px-2 lg:px-8">
-        <div className="flex bg-yellow-200 h-16 justify-between">
+        <div className="flex h-16 justify-between">
           <div className="flex items-center">
             <button
               onClick={() => setSidebarOpen(true)}
-              className="lg:hidden text-gray-500 hover:text-gray-700 mr-4"
+              className=" text-gray-500 hover:text-gray-700 mr-4"
             >
               {""}
               <Menu className="w-6 h-6" />
